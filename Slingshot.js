aba92fb6cc7424ddbbbdd38f556925f2701e5745
Devesh-gif-dot <deveshmarya@gmail.com>
2020-10-19 16:44:04 +0530

    AngryBirdsStage4

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
     }
 
     fly(){
@@ -16,11 +19,24 @@ class SlingShot{
     }
 
     display(){
+
+        image(this.sling2,170,20);
+        image(this.sling1,200,20);
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+                    
+            if(pointA.x > 200){ 
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30)        
+            }
+            else{
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30) 
+            }
+            strokeWeight(15);
+            stroke(48,22,8);
+            line(pointA.x-20, pointA.y, pointB.x, pointB.y);
+            line(pointA.x+20, pointA.y,pointB.x+50,pointB.y-10);
         }
     }
     
